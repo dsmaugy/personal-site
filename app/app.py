53928fa5e00f898214d412a6c8e9bcf19cb57deb
Darwin <darwin78913@gmail.com>
2021-07-22 23:42:19 -0400

    add more boilerplate and letterboxd parser

--- app/app.py
+++ app/app.py
@@ -1,5 +1,42 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+
+import defusedxml.ElementTree as ET
+import requests
+import re
+
+
 app = Flask(__name__)
+
+LTRBXD_RSS = "https://letterboxd.com/dsmaugy/rss"
+
+def get_last_movie():
+    # TODO do caching here
+    movies = requests.get(LTRBXD_RSS)
+
+    movies_root = ET.fromstring(movies.content)
+
+    first_movie = movies_root[0].findall('item')[0]
+    movie_fulltitle = first_movie.find('title').text
+    splitter = re.search(", [0-9]*", movie_fulltitle).group()
+    movie_title = movie_fulltitle[0:movie_fulltitle.index(splitter)]
+
+    movie_year = splitter.strip(', ')
+
+    movie_stars = movie_fulltitle[len(movie_title + splitter):].strip(' -')
+    movie_rating = len(movie_stars) - (0.5 * (movie_stars[-1] == '½'))
+
+    movie_portrait = re.search("https://.*\.jpg", first_movie.find("description").text).group()
+
+    return {'title': movie_title, 'year': movie_year, 'rating': movie_rating, 'portrait': movie_portrait}
+
+@app.route('/movies')
+def testing():
+    return get_last_movie()
+
+@app.route('/html')
+def html_test():
+    return render_template("index.html")
+
 
 @app.route('/')
 def index():
