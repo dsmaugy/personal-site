6cf7d7dbd9b27d86d9747095cc81569275cf3d12
Darwin <darwin78913@gmail.com>
2021-07-25 20:15:19 -0400

    add callback endpoint

--- app/app.py
+++ app/app.py
@@ -66,10 +66,14 @@ def last_songs_view():
 def html_test():
     return render_template("index.html")
 
+@app.route('/spotify_callback')
+def spotify_callback():
+    return "Ok"
+
 
 @app.route('/')
 def index():
     return "<h1>Under Construction!</h1>"
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run("0.0.0.0", port=5000)
