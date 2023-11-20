1517920941e524d7f374ad5b5094d00f225f79c7
Darwin Do <darwin78913@gmail.com>
2023-11-20 14:23:29 -0500

    update requirements

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,5 +1,6 @@
 #!/bin/fish
 
 #  will be exposed on localhost:5000
-poetry run pip3 freeze > requirements.txt
+#poetry run pip3 freeze > requirements.txt
+poetry export -f requirements.txt --output requirements.txt
 poetry run heroku local
