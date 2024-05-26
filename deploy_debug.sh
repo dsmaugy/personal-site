eb578938671678da99c286cb65d43263b49463e1
Darwin Do <darwin78913@gmail.com>
2024-05-25 21:58:47 -0400

    clean out python files

--- deploy_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/fish
-
-export FLASK_APP=app/app.py
-poetry run flask run --debug
--- deploy_local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/fish
-
-#  will be exposed on localhost:5000
-#poetry run pip3 freeze > requirements.txt
-poetry export -f requirements.txt --output requirements.txt
-poetry run heroku local
