94186a3b7d716b2c32a732668f6c0be73002398f
Darwin Do <darwin78913@gmail.com>
2024-06-06 14:55:37 -0400

    add spotify preview

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,6 @@
+let shouldPlay = false;
+let trackMap = new Map();
+let currentlyPlaying = null;
 
 function carouselItemEntered(item) {
     let tooltipElement = item.getElementsByClassName("carousel-tooltip");
@@ -10,4 +13,35 @@ function carouselItemLeft(item) {
     let tooltipArea = document.getElementById("left-panel-tooltip");
 
     tooltipArea.innerHTML = "";
+}
+
+function togglePreview() {
+    shouldPlay = !shouldPlay;
+    toggleButton = document.getElementById("spotify-preview-toggle");
+
+    if (shouldPlay) {
+        let links = document.getElementsByClassName("spotify-preview-link")
+        for (let trackPreview of links) {
+            let audioElement = new Audio(trackPreview.innerHTML);
+            trackMap.set(trackPreview.innerHTML, audioElement);
+        }
+
+        toggleButton.style.background = "lightgreen";
+    } else {
+        toggleButton.style.background = "aliceblue";
+    }
+}
+
+function previewSpotify(item) {
+    if (shouldPlay) {
+        let url = item.getElementsByClassName("spotify-preview-link")[0].innerHTML;
+        currentlyPlaying = trackMap.get(url);
+        currentlyPlaying.play();
+    }
+}
+
+function stopSpotify() {
+    if (currentlyPlaying !== null) {
+        currentlyPlaying.pause();
+    }
 }
